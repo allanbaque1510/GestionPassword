e5da453b1f27ed1334454df026fc5cb9cb9a0515
allanbaque1510 <allanbaque1510@gmail.com>
2023-08-23 22:38:13 -0500

    Se agregaron rutas

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const TOKEN_SECRET = "create secret key";
--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() =>{
     try {
-        await mongoose.connect('mongodb://localhost/gestionDB')
+        await mongoose.connect('mongodb://127.0.0.1/gestionDB')
         console.log(">>>>DB is connected")
     } catch (error) {
         console.log(error)    
